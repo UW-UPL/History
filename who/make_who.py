9453b3de8d1f45d2e7a4a1d16a2903c19ca4cff8
LOZORD <ljrudberg@gmail.com>
2016-04-22 14:44:16 -0500

    Started work on 'who'

---
+++ who/make_who.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import json
+
+with open('./who.json', 'r') as who_file:
+  # first load the data
+  json_data = json.load(who_file)
+  who_data = json_data['who']
+
+  # then sort it by (1) start date (2) username
+
+  # TODO: might need to map start date to something more suited for sorting
+  by_start_date  = lambda user: user['start']
+  by_username    = lambda user: user['username']
+
+  # sorted_data = sorted((sorted(json_data, key=by_start_date), key=by_username)
+  sorted_data = sorted(who_data, key=by_username)
+  sorted_data = sorted(sorted_data, key=by_start_date)
+
+with open('./who.markdown', 'w') as markdown_file:
+  table_header  = '| Username | Name | Start | End | Coord? | Jobs | Link | Misc. |\n'
+  table_header += '| ---------|------|-------|-----|--------|------|------|------ |\n'
+
+  table_header = table_header.strip()
+
+  markdown_file.write(table_header)
+
+  for user in sorted_data:
+    coord_emoji = None
+    if user['coord']:
+      coord_emoji = ':white_check_mark:'
+    else:
+      coord_emoji = ':x:'
+    row_entry = '\n| `%s` | %s | %s | %s | %s | %s | %s | %s |'
+    row_entry %= (user['username'], user['name'], user['start'], user['end'],
+      coord_emoji, user['jobs'], user['link'], user['misc'])
+
+    markdown_file.write(row_entry)
---
+++ who/who.markdown
@@ -0,0 +1,4 @@
+| Username | Name | Start | End | Coord? | Jobs | Link | Misc. |
+| ---------|------|-------|-----|--------|------|------|------ |
+| `frankenstein` | Doctor Frankenstein | Fall 1900 | Spring 1904 | :white_check_mark: | Doctor, Mad Scientist, NetScape | http://example.com | Created a monster |
+| `bloodsucker` | Count Dracula | Fall 1901 | Fall 1902 | :x: | Count, Vampire, Sun Microsystems | http://wisc.edu |  |
